4616b3b6bd70b969d3cec8ca7baace06e7b5303e
LooMingYea <93505038+LooMingYea@users.noreply.github.com>
2023-02-21 15:38:58 +0800

    update

---
+++ Assets/test.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public void StartHitting()
+    {
+
+    }
+    public void FinishHitting()
+    {
+
+    }
+}
